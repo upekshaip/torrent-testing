d1e05bb7cb5af0cd687b7ff13ab7ecf43bdd3a4e
upekshaip <upekshaip@gmail.com>
2022-11-17 15:32:32 +0530

    2nd

--- test.py
+++ test.py
@@ -9,10 +9,11 @@ ses = lt.session()
 ses.listen_on(6881, 6891)
 
 params = {"save_path": "/dowload/",
-          "storage_mod": lt.storage_mode_t(2),
-          "paused": False,
-          "auto_managed": True,
-          "duplicate_is_error": True}
+          "storage_mode": lt.storage_mode_t(2)
+          #   "paused": False,
+          #   "auto_managed": True,
+          #   "duplicate_is_error": True
+          }
 
 print(link)
 
@@ -32,7 +33,7 @@ while (handle.status().state != lt.torrent_status.seeding):
     s = handle.status()
     state_str = ["queued", "checking", "downloading metadata",
                  "downloading", "finished", "seeding", "allocating"]
-    print("%.2f%% complete (down: %.1f kb/s up: %.1f kb/s peers: %d) %s" % (s.progress() *
+    print("%.2f%% complete (down: %.1f kb/s up: %.1f kb/s peers: %d) %s" % (s.progress *
           100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
     time.sleep(5)
 
